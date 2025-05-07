8a3211fd792ea9b3e79f971cc3052d1bbba3f48f
Bruno Laitano <bruno.grigoletti.laitano@gmail.com>
2025-05-07 20:44:14 -0300

    Microservices are ready

---
+++ dem/src/test/java/mdm/engsoft2/dem/DemApplicationTests.java
@@ -0,0 +1,13 @@
+package mdm.engsoft2.dem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ mdm/src/test/java/mdm/engsoft2/mdm/MdmApplicationTests.java
@@ -0,0 +1,13 @@
+package mdm.engsoft2.mdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
