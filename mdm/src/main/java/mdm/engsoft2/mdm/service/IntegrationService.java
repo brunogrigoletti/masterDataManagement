6c29a98b7cb5d710834aa9ed7ab6f36db166270a
Bruno Laitano <bruno.grigoletti.laitano@gmail.com>
2025-05-12 19:59:09 -0300

    Docker is working!

--- mdm/src/main/java/mdm/engsoft2/mdm/service/IntegrationService.java
+++ mdm/src/main/java/mdm/engsoft2/mdm/service/IntegrationService.java
@@ -2,6 +2,7 @@ package mdm.engsoft2.mdm.service;
 
 import mdm.engsoft2.mdm.entity.CountryEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 import java.util.Arrays;
 import java.util.List;
@@ -17,8 +18,13 @@ public class IntegrationService {
     }
 
     public List<CountryEntity> fetchCountriesFromDem() {
-        String url = "http://localhost:8080/countries";
-        CountryEntity[] countriesArray = restTemplate.getForObject(url, CountryEntity[].class);
-        return Arrays.stream(countriesArray).collect(Collectors.toList());
+        try {
+            String url = "http://dem:8080/countries";
+            CountryEntity[] countriesArray = restTemplate.getForObject(url, CountryEntity[].class);
+            return Arrays.stream(countriesArray).collect(Collectors.toList());
+        } catch (ResourceAccessException e) {
+            System.err.println("Error fetching countries: " + e.getMessage());
+            return List.of();
+        }
     }
 }
